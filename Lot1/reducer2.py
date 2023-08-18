ebd6df7e02f74a2e4cdbd46f1bb9ae833c8d3032
Alexis Blas <ablas@diginamic-formation.fr>
2023-08-18 12:29:47 +0200

    triage et sélection des 10 premiers

--- Lot1/reducer2.py
+++ Lot1/reducer2.py
@@ -1,9 +1,11 @@
+import heapq
 import sys
 
 current_name = None
 current_city = None
 current_department = None
 current_nbcolis = 0
+list = []
 
 for line in sys.stdin:
     line = line.strip()
@@ -15,17 +17,16 @@ for line in sys.stdin:
 
     if current_name is None:
         current_name = name
+        current_city = city
+        current_department = department
         current_nbcolis = nbcolis
 
     if current_name == name:
         current_nbcolis += nbcolis
 
-        if current_nbcolis == nbcolis:
-            current_nbcolis += nbcolis
-
     else:
         if current_name:
-            print('%s\t%s\t%s\t%s' % (current_name, current_city, current_department, current_nbcolis))
+            list.append((current_name, current_city, current_department, current_nbcolis))
 
         current_name = name
         current_city = city
@@ -33,4 +34,10 @@ for line in sys.stdin:
         current_nbcolis = nbcolis
 
 if current_name:
-    print('%s\t%s\t%s\t%s' % (current_name, current_city, current_department, current_nbcolis))
+    list.append((current_name, current_city, current_department, current_nbcolis))
+
+# Utilisation de heapq.nlargest pour obtenir les 10 premiers clients les plus fidèles
+top_clients = heapq.nlargest(10, list, key=lambda x: x[3])
+
+for client in top_clients:
+    print('%s\t%s\t%s\t%s' % (client[0], client[1], client[2], client[3]))  # Impression des valeurs des clients fidèles
