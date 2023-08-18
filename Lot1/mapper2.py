8c0ff822fe5d726bd1fd40e61e8d2837ab9264b2
Alexis Blas <ablas@diginamic-formation.fr>
2023-08-18 10:31:18 +0200

    Ajout name dans print et ajout commentaire

--- Lot1/mapper2.py
+++ Lot1/mapper2.py
@@ -1,11 +1,7 @@
 import re
 import sys
-import pandas as pd
 
 i = 0
-'''df = pd.read_csv('C:/Users/FORMATION/Desktop/Projet Hadoop/dataw_fro03.csv')
-df['prenomcli'] = df['prenomcli'].str.replace(',', ' ')
-df.to_csv("data1.csv",index = False)'''
 
 
 for line in sys.stdin:
@@ -14,9 +10,10 @@ for line in sys.stdin:
         line = line.strip()
         fields = re.split(r'",|L,',line)
         year = int(fields[7].replace('"', '').split('-')[0]) if (fields[7].replace('"', '').split('-')[0]).isdecimal() else 0
+        name = fields[0].replace('"', '') + " "+ fields[2].replace('"', '')
         nbcolis = int(fields[10].replace('"', '')) if fields[10].replace('"', '') != 'NUL' else 0
         city = fields[5].replace('"', '')
         department = int(fields[4][:3].replace('"', '')) if fields[4][:3].replace('"', '').isdecimal() else 0
         if year >= 2008 and nbcolis != 0:
-            print('%s,%s,%s' % (city, department, nbcolis))
-    i += 1
+            print('%s, %s, %s, %s' % (name, city, department, nbcolis))
+    i += 1 # nous voulons sauter la première ligne car il s'agit du nom des colonnes.
