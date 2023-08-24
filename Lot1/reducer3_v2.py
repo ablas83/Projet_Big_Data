e2357c7f60da376efbcd9abb0d53cd76a783dbce
Mohammed <mohamedouahmane020@gmail.com>
2023-08-24 10:12:14 +0200

    Lot1 + Lot2 final

--- Lot1/reducer3_v2.py
+++ Lot1/reducer3_v2.py
@@ -2,78 +2,85 @@ import sys
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-
+# Initialisation des variables
 current_objet = None
 current_year = None
 current_department = None
+current_qte = 0
 
 year = None
 objet = None
 department = None
+qte = 0
 
-dict_annee_nbr_com_par_objet = {}
-list_dict = []
-current_nbr_commande = 0
+dict_année_nbr_com_par_objet = {}  # Dictionnaire pour stocker les données par objet
+list_dict = []  # Liste temporaire pour stocker les données par année et département
 
 for line in sys.stdin:
-    # remove leading and trailing whitespace
+    # Suppression des espaces en début et en fin de ligne
     line = line.strip()
-    # parse the input we got from mapper.py
-    objet, year, department, code_commande, nbcolis = line.split(',')
+    # Analyse de l'entrée provenant de mapper.py
+    objet, year, department, code_commande, qte = line.split(',')
+
+    try:
+        qte = float(qte)  # Conversion de la quantité en nombre à virgule flottante
+    except ValueError:
+        continue  # Ignorer les lignes avec des quantités non valides
 
     if current_objet is None:
+        # Initialisation des valeurs pour le premier objet
         current_objet = objet
         current_department = department
         current_year = year
-        current_nbr_commande = 1
+        current_qte = qte
     elif current_objet == objet:
         if current_year == year:
             if current_department == department:
-                current_nbr_commande += 1
+                current_qte += qte  # Ajout de la quantité à la quantité actuelle
             else:
-                list_dict.append((current_year, current_department, current_nbr_commande))
+                list_dict.append((current_year, current_department, current_qte))
                 current_year = year
                 current_department = department
-                current_nbr_commande = 1
+                current_qte = qte
         else:
-            list_dict.append((current_year, current_department, current_nbr_commande))
+            list_dict.append((current_year, current_department, current_qte))
             current_year = year
             current_department = department
-            current_nbr_commande = 1
-
+            current_qte = qte
     else:
-        list_dict.append((current_year, current_department, current_nbr_commande))
-        dict_annee_nbr_com_par_objet[current_objet] = list_dict
+        list_dict.append((current_year, current_department, current_qte))
+        dict_année_nbr_com_par_objet[current_objet] = list_dict
         current_objet = objet
         current_year = year
         current_department = department
-        current_nbr_commande = 1
+        current_qte = qte
         list_dict = []
 
 if current_objet == objet:
-    list_dict.append((current_year, current_department, current_nbr_commande))
-    dict_annee_nbr_com_par_objet[current_objet] = list_dict
+    list_dict.append((current_year, current_department, current_qte))
+    dict_année_nbr_com_par_objet[current_objet] = list_dict
 
 pdf_filename = 'plot_evolution_par_objet_departement.pdf'  # Nom du fichier PDF à générer
 
+# Création du fichier PDF avec les graphiques
 with PdfPages(pdf_filename) as pdf:
-    for object_name, data_points in dict_annee_nbr_com_par_objet.items():
+    for object_name, data_points in dict_année_nbr_com_par_objet.items():
         departments = set([department for _, department, _ in data_points])
 
         for department in departments:
-            plt.figure(figsize=(8, 5))  # Ajustez la taille de la figure si nécessaire
+            plt.figure(figsize=(8, 5))  # Ajuster la taille de la figure si nécessaire
 
-            filtered_data = [(year, nbr_commande) for year, dep, nbr_commande in data_points if dep == department]
-            years, nbr_commande = zip(*filtered_data)
+            filtered_data = [(year, nbr_objets) for year, dep, nbr_objets in data_points if dep == department]
+            years, nbr_objets = zip(*filtered_data)
 
-            plt.plot(years, nbr_commande, marker='o', label=f'Department {department}')
+            plt.plot(years, nbr_objets, marker='o', label=f'Department {department}')
             plt.xlabel('Année')
-            plt.ylabel('Nombre de commandes')
-            plt.title(f'Évolution du nombre de commandes pour {object_name} - Department {department}')
+            plt.ylabel('Nombre d\'objets')
+            plt.title(f'Évolution du nombre d\'objets pour {object_name} - Département {department}')
             plt.legend()
             plt.grid(True)
             plt.xticks(rotation=45)
             plt.tight_layout()
 
-            pdf.savefig()
-            plt.close()
+            pdf.savefig()  # Sauvegarde du graphique dans le PDF
+            plt.close()  # Fermeture de la figure après sauvegarde
--- Lot2/reducer1.py
+++ Lot2/reducer1.py
@@ -1,7 +1,7 @@
 import sys
-from operator import itemgetter
-import openpyxl
+import pandas as pd
 
+# Initialisation des variables
 current_codecde = None
 current_ville = None
 current_nbr_timbrecde = 0
@@ -12,28 +12,30 @@ ville = None
 nbr_timbrecde = 0
 nbr_colis = 0
 
-list_codecde = [[]]
+list_codecde = [[]]  # Initialisation d'une liste de listes (peut-être pour éviter une première comparaison)
 
+# Parcours des lignes d'entrée
 for line in sys.stdin:
-    # remove leading and trailing whitespace
+    # Suppression des espaces en début et en fin de ligne
     line = line.strip()
-    # parse the input we got from mapper.py
+    # Analyse de l'entrée provenant de mapper.py
     codecde, ville, nbr_colis, nbr_timbrecde = line.split(',')
+
     try:
-        nbr_colis = float(nbr_colis)
+        nbr_colis = float(nbr_colis)  # Conversion en nombre à virgule flottante
         nbr_timbrecde = float(nbr_timbrecde)
     except ValueError:
-        continue
+        continue  # Ignorer les lignes avec des valeurs non valides
 
     if current_codecde is None:
+        # Initialisation des valeurs pour la première commande
         current_codecde = codecde
         current_ville = ville
         current_nbr_colis = nbr_colis
         current_nbr_timbrecde = nbr_timbrecde
 
-    elif current_codecde == codecde :
-
-        current_nbr_timbrecde += nbr_timbrecde
+    elif current_codecde == codecde:
+        current_nbr_timbrecde += nbr_timbrecde  # Ajout du timbre cde à la commande actuelle
 
     else:
         list_codecde.append([current_codecde, current_ville, current_nbr_colis, round(current_nbr_timbrecde, 2)])
@@ -45,31 +47,17 @@ for line in sys.stdin:
 if current_codecde:
     list_codecde.append([current_codecde, current_ville, current_nbr_colis, round(current_nbr_timbrecde, 2)])
 
+list_codecde.remove([])  # Suppression de la liste vide éventuellement générée en début
 
-list_codecde.remove([])
+sorted_list_codecde = sorted(list_codecde, key=lambda x: x[2], reverse=True)[:100]
 
-sorted_list_codecde = sorted(list_codecde, key=itemgetter(2), reverse=True)[:100]
+# Création d'un DataFrame pandas
+df = pd.DataFrame(sorted_list_codecde, columns=['Code Commande', 'Ville', 'Nbr Colis', 'Somme timbrecde'])
 
-# Créer un nouveau classeur Excel
-workbook = openpyxl.Workbook()
-lot2_ex1 = workbook.active
+# Définition du nom du fichier Excel
+excel_filename = 'lot2_ex1.xlsx'
 
-# En-tête des colonnes
-header = ['Code Commande', 'Ville', 'Nbr Colis', 'Somme timbrecde']
+# Sauvegarde du DataFrame dans un fichier Excel
+df.to_excel(excel_filename, index=False)
 
-lot2_ex1.append(header)
-# Ajouter les données à la feuille Excel
-for row in sorted_list_codecde:
-    lot2_ex1.append(row)
-
-# Sauvegarder le classeur Excel
-excel_file_path = 'lot2_ex1.xlsx'
-workbook.save(excel_file_path)
-
-'''with open("output4.txt", "a") as f:
-    for line in sorted_list_codecde:
-        print(line,file=f)'''
-
-
-
-
+print(f"DataFrame saved to '{excel_filename}'.")
